da898cf704bf191855c36b5a3e5205c29061cc6f
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-04-05 12:18:03 +0500

    changing root dir

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
-import { UsersModule } from 'src/users/users.module';
+import { UsersModule } from './users/users.module';
 
 /**
  * The AppModule is the root module of the NestJS application. It imports other modules, declares controllers, and provides services that can be used throughout the application.
